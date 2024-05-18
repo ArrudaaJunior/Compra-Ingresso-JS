155592332a943c986631c565a529aeec925e9fa0
Arruda Junior <juniorarruda15@gmail.com>
2024-05-18 13:36:50 -0300

    Realizando compra para tipo pista

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 function comprar(){
     let tipo = document.getElementById('tipo-ingresso');
-    let quantidade = document.getElementById('qtd');
+    let quantidade = parseInt(document.getElementById('qtd').value);
+
+    if(tipo.value == 'pista') {
+        comprarPista(quantidade);
+    }
+}
+
+function comprarPista(quantidade) {
+    let quantidadePista = parseInt(document.getElementById('qtd-pista').textContent);
+    if(quantidade > quantidadePista){
+        alert('Quantidade indisponível para tipo posta');
+    } else {
+        quantidadePista = quantidadePista - quantidade;
+        document.getElementById('qtd-pista').textContent = quantidadePista;
+        alert('Compra com sucesso!');
+    }
 }
